a24dfc9f729de372038a8c03064d797aa561789f
Alpha <alpha@pokesplash.net>
2015-10-07 00:34:20 -0400

    Initialized structure and added a setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+
+setup(name='ansible-graph',
+      version='1.0.0',
+      description='Graph-Oriented Ansible Runner',
+      url='http://github.com/Alphadelta14/python-ansible-graph',
+      author='Alpha',
+      author_email='alpha@projectpokemon.org',
+      license='MIT',
+      packages=['ansible', 'ansible.graph'],
+      zip_safe=True)
